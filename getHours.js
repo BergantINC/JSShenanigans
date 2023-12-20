0b9e46bbb5140e30cac35715772608f9ca454f63
Nejc Bergant <nejc.bergant@b-s.si>
2023-12-20 08:28:22 +0100

    Minor tweeks

--- getHours.js
+++ getHours.js
@@ -1,5 +1,6 @@
 //Navodila: kodo kopiraš v konzolo na linku https://allforteam.b-s.si/employee-hours/ , deluje samo za tisti mesec ki je nastavljen trenutno
 var divs = document.getElementsByClassName("day-saldo");
+var date = document.getElementsByClassName("value");
 var sum = 0.0;
 var meant = 0.0;
 //if(hours) zato da trenutnega dneva ne vzame (da vidim koliko mam bonusa ta mesec brez danes ker se nisem koncal)
@@ -15,10 +16,9 @@ for (var i = 0; i < divs.length; i++){
       }
     }
 }
-//console.log(sum);
+//console.log(sum);R
 var diff = sum - meant;
 var hours = parseInt(diff);
 var mins = (diff - hours)*60;
-var saldo = sum + mins;
-console.log("Hours: " + hours + " Mins: " + mins);
-console.log("Saldo: " + saldo);
+console.log("Mesec: " + date[0].innerHTML + " Hours: " + hours + " Mins: " + Math.round(mins));
+console.log("Saldo: " + sum);
