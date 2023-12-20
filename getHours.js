11931e09c605cce3ec5d11c9237db03c57a82ac3
Nejc Bergant <nejc.bergant@b-s.si>
2023-12-20 14:38:45 +0100

    Days Left

--- getHours.js
+++ getHours.js
@@ -1,9 +1,11 @@
 //Navodila: kodo kopiraš v konzolo na linku https://allforteam.b-s.si/employee-hours/ , deluje samo za tisti mesec ki je nastavljen trenutno
 var divs = document.getElementsByClassName("day-saldo");
 var date = document.getElementsByClassName("value");
+var daysToWork = ["pon.","tor.","sre.","čet.","pet."];
 var sum = 0.0;
 var meant = 0.0;
 //if(hours) zato da trenutnega dneva ne vzame (da vidim koliko mam bonusa ta mesec brez danes ker se nisem koncal)
+var workedDays = 0;
 for (var i = 0; i < divs.length; i++){
   var div = divs[i].innerHTML;
   if(div != "+0:00"){
@@ -11,14 +13,26 @@ for (var i = 0; i < divs.length; i++){
     if(hours == "+7" || hours == "+8"){
         sum += parseFloat(hours);
         meant += 8;
+        workedDays += 1;
         if(mins != "00")
             sum += (parseFloat(mins)/60);
       }
     }
 }
-//console.log(sum);R
 var diff = sum - meant;
 var hours = parseInt(diff);
 var mins = (diff - hours)*60;
 console.log("Mesec: " + date[0].innerHTML + " Hours: " + hours + " Mins: " + Math.round(mins));
 console.log("Saldo: " + sum);
+
+//Days to work in a month
+var daysToWork = ["pon.","tor.","sre.","čet.","pet."];
+var daysInMonth = document.getElementsByClassName("head-1");
+var sumOfDays = 0;
+for (var i = 0; i < daysInMonth.length; i++){
+  var day = daysInMonth[i].innerHTML.split(">")[2];
+  if(daysToWork.Contains(day)){
+    sumOfDays += 1;
+     }
+}
+console.log("I have to work for "+sumOfDays+" days this month. Leftover: " + (sumOfDays - workedDays));
